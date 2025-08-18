8e1f4d75d4ce52ca197e1a09174846fa53ed6de7
Tihohod2000 <dumanovskiy15@gmail.com>
2025-08-19 01:46:28 +0300

    fix duplicate

--- Advertising_platforms/AdvertisingPlatforms.cs
+++ Advertising_platforms/AdvertisingPlatforms.cs
@@ -1,19 +1,78 @@
 namespace Advertising_platforms;
 
-public class AdvertisingPlatforms
+public static class AdvertisingPlatforms
 {
-    public static Dictionary<string, List<string>> Platforms { get; set; } = new Dictionary<string, List<string>>();
+    public static Dictionary<string, List<string>> AdvertisingPlatformsHash { get; private set; } = new Dictionary<string, List<string>>();
 
     public static void AddPlatform(string local, string name)
     {
-        if (Platforms.ContainsKey(local))
+        if (AdvertisingPlatformsHash.ContainsKey(local))
         {
+            List<string> platforms = AdvertisingPlatformsHash[local];
+            foreach (var platform in platforms)
+            {
+                if (platform == name)
+                {
+                    return;
+                }
+            }
             
-            Platforms[local].Add(name);
+            AdvertisingPlatformsHash[local].Add(name);
         }
         else
         {
-            Platforms[local] = new List<string> { name };
+            AdvertisingPlatformsHash[local] = new List<string> { name };
         }
+    }
+
+    public static async Task<string[]> ReadInfoFromFile(IFormFile file)
+    {
+        using (var reader = new StreamReader(file.OpenReadStream()))
+        {
+            string fileContent = await reader.ReadToEndAsync();
+            fileContent = fileContent.Replace("\r", "");
+
+            string[] ads = fileContent.Split("\n");
+
+            foreach (var curr in ads)
+            {
+                string name = curr.Split(":")[0];
+                string[] locals = curr.Split(":")[1].Split(",");
+                for(byte i = 0; i < locals.Length; i++)
+                {
+
+                    AddPlatform(locals[i], name);
+
+                    while (true)
+                    {
+
+                        int lastSlashIndex = locals[i].LastIndexOf('/');
+
+                        if (lastSlashIndex == 0)
+                        {
+                            break;
+                        }
+
+                        if (lastSlashIndex > 0) 
+                        {
+                            locals[i] = locals[i].Substring(0, lastSlashIndex);
+                            AddPlatform(locals[i], name);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+            }
+            
+            return ads;
+        }
+    }
+
+    public static async Task ClearDictionary()
+    {
+        AdvertisingPlatformsHash.Clear();
     }
 }
--- Advertising_platforms/Controllers/adPlatformsController.cs
+++ Advertising_platforms/Controllers/adPlatformsController.cs
@@ -7,20 +7,24 @@ namespace Advertising_platforms.Controllers;
 [Route("api/[controller]")]
 public class adPlatformsController : ControllerBase
 {
-    
     [HttpGet("search")]
-    public IActionResult Search()
+    public async Task<IActionResult> Search()
     {
         string location = HttpContext.Request.Query["location"].ToString();
 
-        var search = AdvertisingPlatforms.Platforms[location];
-        
-        
-        return Ok(new
+        try
         {
-            search
-        });
-        
+            var search = AdvertisingPlatforms.AdvertisingPlatformsHash[location];
+            return Ok(new
+            {
+                search
+            });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return NotFound($"Данные по локации {location} не найдены!");
+        }
     }
 
 
@@ -33,59 +37,25 @@ public class adPlatformsController : ControllerBase
             return BadRequest("No file uploaded.");
         }
 
-        var file = Request.Form.Files[0];
-        
+
+        IFormFile file = Request.Form.Files[0];
+
+
         if (Path.GetExtension(file.FileName).ToLower() != ".txt")
         {
-            return BadRequest("Only .txt files.");
+            return BadRequest("Only .txt file.");
         }
-        
-        using (var reader = new StreamReader(file.OpenReadStream()))
+
+        await AdvertisingPlatforms.ClearDictionary();
+        string[] fileContent = await AdvertisingPlatforms.ReadInfoFromFile(file);
+
+
+        return Ok(new
         {
-            string fileContent = await reader.ReadToEndAsync();
-            fileContent = fileContent.Replace("\r", "");
-
-            string[] ads = fileContent.Split("\n");
-
-            foreach (var curr in ads)
-            {
-                string name = curr.Split(":")[0];
-                string[] locals = curr.Split(":")[1].Split(",");
-                for(byte i = 0; i < locals.Length; i++)
-                {
-
-                    AdvertisingPlatforms.AddPlatform(locals[i], name);
-
-                    while (true)
-                    {
-
-                        int lastSlashIndex = locals[i].LastIndexOf('/');
-
-                        if (lastSlashIndex == 0)
-                        {
-                            break;
-                        }
-
-                        if (lastSlashIndex > 0) 
-                        {
-                            locals[i] = locals[i].Substring(0, lastSlashIndex);
-                            AdvertisingPlatforms.AddPlatform(locals[i], name);
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-            }
-            
-            return Ok(new {
-                Platforms = AdvertisingPlatforms.Platforms,
-                FileName = file.FileName,
-                Content = fileContent,
-                Size = file.Length
-            });
-        }
+            FileName = file.FileName,
+            Size = file.Length,
+            Content = fileContent,
+            Platforms = AdvertisingPlatforms.AdvertisingPlatformsHash
+        });
     }
 }
